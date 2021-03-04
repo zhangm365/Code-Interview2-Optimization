700af6d41676abb204a4699468c383771cb8b9d5
zhangm365 <mzhang2048@gmail.com>
2021-03-04 21:06:21 +0800

    bst_lca_iterative

---
+++ tree/LCA/BST_LCA_Iterative.cpp
@@ -0,0 +1,33 @@
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        
+
+        if(!root) return nullptr;
+
+        while( root )
+        {
+            
+            if( p->val < root->val && q->val < root->val )  // goto the left
+                root = root->left;
+            else if ( p->val > root->val && q->val > root->val )    // goto the right
+                root = root->right;
+            else 
+                break;
+
+        }
+
+        return root;
+        
+    }
+};
